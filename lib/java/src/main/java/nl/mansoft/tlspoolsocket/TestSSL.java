015bb913d4dddece0dcc761416ceaced30c4f504
Henri Manson <hfmanson@gmail.com>
2019-04-25 09:43:28 +0200

    Start testing SSLSession

--- lib/java/src/main/java/nl/mansoft/tlspoolsocket/TestSSL.java
+++ lib/java/src/main/java/nl/mansoft/tlspoolsocket/TestSSL.java
@@ -14,59 +14,67 @@ import java.util.logging.Logger;
 import javax.net.ssl.HttpsURLConnection;
 
 public class TestSSL {
-    public static void direct(InputStream is) throws IOException {
-        byte[] buf = new byte[1024*1024];
-        int bytesread;
-        int offset = 0;
-        while ((bytesread = is.read(buf, offset, buf.length - offset)) > 0) {
-            System.err.println("read: " + bytesread);
-            offset += bytesread;
-        }
-        System.err.println("total size: " + offset);
-        System.out.write(buf, 0, offset);
-    }
+	public static void direct(InputStream is) throws IOException {
+		byte[] buf = new byte[1024*1024];
+		int bytesread;
+		int offset = 0;
+		while ((bytesread = is.read(buf, offset, buf.length - offset)) > 0) {
+			System.err.println("read: " + bytesread);
+			offset += bytesread;
+		}
+		System.err.println("total size: " + offset);
+		System.out.write(buf, 0, offset);
+	}
 
-    public static void printCertificates(HttpsURLConnection httpsURLConnection) {
-        if (httpsURLConnection != null) {
-            System.out.println(httpsURLConnection.getCipherSuite());
-            Certificate certificates[] = httpsURLConnection.getLocalCertificates();
-            if (certificates != null) {
-                for (Certificate certificate : certificates) {
-                    System.out.println(certificate);
-                }
-            }
-        }
-    }
+	public static void printCertificates(HttpsURLConnection httpsURLConnection) {
+		if (httpsURLConnection != null) {
+			//System.out.println(httpsURLConnection.getCipherSuite());
+			try {
+				Certificate certificates[] = httpsURLConnection.getServerCertificates();
+				if (certificates != null) {
+					for (Certificate certificate : certificates) {
+						System.out.println(certificate);
+					}
+				} else {
+					System.out.println("No server certificates");
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
-    public static void printResponseHeaders(Map<String, List<String>> headerFields) {
-        for (Map.Entry<String, List<String>> e: headerFields.entrySet()) {
-            System.err.println(e.getKey());
-            for (String value : e.getValue()) {
-                System.err.println("\t" + value);
-            }
-        }
-    }
+	public static void printResponseHeaders(Map<String, List<String>> headerFields) {
+		for (Map.Entry<String, List<String>> e: headerFields.entrySet()) {
+			System.err.println(e.getKey());
+			for (String value : e.getValue()) {
+				System.err.println("\t" + value);
+			}
+		}
+	}
 
-    public static void testSSL(String urlString, boolean useTlspool) {
-        try {
-            System.err.println("Visiting: " + urlString);
-            URL url = new URL(urlString);
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            if (urlConnection instanceof HttpsURLConnection) {
-                HttpsURLConnection httpsURLConnection = (HttpsURLConnection) urlConnection;
-                if (useTlspool) {
-                    httpsURLConnection.setSSLSocketFactory(new TlspoolSSLSocketFactory());
-                }
-                //printCertificates(httpsURLConnection);
-            }
-            urlConnection.connect();
-            printResponseHeaders(urlConnection.getHeaderFields());
-            direct(urlConnection.getInputStream());
-            urlConnection.disconnect();
-        } catch (Exception ex) {
-            Logger.getLogger(TestSSL.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+	public static void testSSL(String urlString, boolean useTlspool) {
+		try {
+			System.err.println("Visiting: " + urlString);
+			URL url = new URL(urlString);
+			HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+			if (urlConnection instanceof HttpsURLConnection) {
+				HttpsURLConnection httpsURLConnection = (HttpsURLConnection) urlConnection;
+				if (useTlspool) {
+					httpsURLConnection.setSSLSocketFactory(new TlspoolSSLSocketFactory());
+				}
+			}
+			urlConnection.connect();
+			if (urlConnection instanceof HttpsURLConnection) {
+				HttpsURLConnection httpsURLConnection = (HttpsURLConnection) urlConnection;
+				printCertificates(httpsURLConnection);
+			}
+			printResponseHeaders(urlConnection.getHeaderFields());
+			direct(urlConnection.getInputStream());
+			urlConnection.disconnect();
+		} catch (Exception ex) {
+			Logger.getLogger(TestSSL.class.getName()).log(Level.SEVERE, null, ex);
+		}
+	}
 }
-
 
